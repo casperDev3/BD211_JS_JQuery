91175f8a203ae85cc3aee6bc4513a9a234444fef
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-01-30 20:34:20 +0200

    add

--- scripts/index.js
+++ scripts/index.js
@@ -1,127 +1,123 @@
-// functions
-function testDeclareFunction(a, b = 2) {
-    const c = a + b
-    if (c > 4) {
-        return "+"
-    } else {
-        return "-"
+class Car {
+    #test = "test"
+    constructor(brand, year) {
+        this.brand = brand;
+        this.year = year;
+        this.#test = year + 2;
+    }
+    //! public method
+    printText(txt) {
+        this.txt = txt;
+        console.log(txt)
+    }
+    changeValTest(val) {
+        this.#test = val;
+    }
+    //! privare method 
+    #sayHello() {
+        console.log("Say Hello!")
+    }
+    // getters
+    get test() {
+        return this.#test;
+    }
+    // setter
+    set newTest(test) {
+        this.#test = test;
     }
 }
 
-function objects() {
-    let someObj = {
-        first_name: "John",
-        last_name: "Doe",
-        age: 21
-    };
-    console.log("NAME:", someObj.first_name);
-    someObj.city = "Dro";
-    delete someObj.age;
-    someObj = {};
-    console.log("T", someObj);
+class Tires extends Car {
+    constructor(brand, year) {
+        super(brand, year)
+    }
+    printText(txt = "custom") {
+        console.log(txt)
+    }
+
 }
 
-const arrFunction = (text) => {
-    console.log("Test Func", text)
+function oop() {
+    const car = new Car("BMW", 2012);
+    const tires = new Tires("Some brand", 2012)
+    console.log("tires", tires.brand, tires.year)
+    console.log(car.brand)
+    car.printText("text")
+    tires.printText()
+    console.log(car.test)
+    car.newTest = 2055
+    console.log(car.test)
+    car.changeValTest(2099)
+    console.log(car.test)
+    console.log(car.txt)
 }
 
-const conditionals = () => {
-    // let age = 4;
-    // if (age >= 18) {
-    //     console.log("adult");
-    // } else if (age >= 0 && age < 18){
-    //     console.log(
-    //         "child", age 
-    //     )
-    // }
-    // // ternal condition
-    // age >= 18 ? console.log("adult") : console.log("child");
+function events() {
+    // const BTN = document.getElementsByTagName("button");
+    // console.log(typeof BTN)
+    // BTN.map((btn)=>{
+    //     btn.addEventListener("click", ()=>{
+    //         alert("test")
+    //     })
+    // })
 
-    // const offline = 0;
-    // if (offline){
-    //     console.log("test off");
-    // }
+    // //! GET ELEMENT BY ID
+    // const BTN = document.getElementById("button")
+    // BTN.addEventListener("click", ()=>{
+    //     alert("test ID")
+    // })
 
-    // let test = 10;
-
-    // test ? (
-    //     console.log(test),
+    // //! GET ELEMENT BY CLASS -- TODO: Need search solution
+    // const BTNs = document.getElementsByClassName("btn");
+    // console.log(typeof BTNs)
+    // BTNs.addEventListener("click", ()=>{
     //     console.log("test")
-    // ) : (
-    //     console.log(test)
-    // )
-
-    // let test = true;
-    // !test && console.log("test &&");
-
-    // !test || console.log("test ||");
-
-    // const expr = 'Papayas';
-    // switch (expr) {
-    //     case 'Oranges':
-    //         console.log('Oranges are $0.59 a pound.');
-    //         break;
-    //     case 'Mangoes':
-    //     case 'Papayas':
-    //         console.log('Mangoes and papayas are $2.79 a pound.');
-    //         // Expected output: "Mangoes and papayas are $2.79 a pound."
-    //         break;
-    //     default:
-    //         console.log(`Sorry, we are out of ${expr}.`);
-    // }
-
-}
-
-
-const loops = () => {
-    // let flag = true;
-    // let counter = 1;
-    // while (flag) {
-    //     if (counter >= 5){
-    //         break;
-    //     }
-    //     counter++;
-    // }
-    // console.log("test",counter)
-
-    let arrValues = [1, "test", "y", false];
-
-    // for (let i = 0; i < arrValues.length; i++){
-    //     console.log(arrValues[i])
-    // }
-
-    // arrValues.forEach((item, index) => {
-    //     if (item) {
-    //         item += 2;
-    //     }
-    //     console.log(item, index)
     // })
 
-    // arrValues.map((item, index) => {
-    //     if (item) {
-    //         item += 2;
-    //     }
-    //     console.log(item, index)
+    // const BTN = document.querySelector("#button")
+    // BTN.addEventListener("click", ()=>{
+    //     alert("query ID")
     // })
 
-    let age = [12, 18, 25, 45, 7];
-    // short form
-    // let adultList = age.filter(item => item >= 18);
-    // full form
-    // let adultList = age.filter((item) => {
-    //     if (item >= 18) {
-    //         return item
-    //     }
+    // const BTNs = document.querySelectorAll(".btn")
+    // BTNs.forEach((btn)=>{
+    //     btn.addEventListener("click", ()=>{
+    //         alert("test")
+    //     })
     // })
-    // console.log(adultList);
-    
+    // console.log(typeof BTNs)
+
+    // // ! Input event
+    // const FIRST_NAME = document.querySelector("#first_name")
+    // FIRST_NAME.addEventListener("input", (event)=>{
+    //     console.log(event.target.value)
+    // })
+
+    // const FORM = document.querySelector("#test_form")
+    // FORM.addEventListener("submit", (e)=>{
+    //     // e.preventDefault();
+    //     const FIRST_NAME = document.querySelector("#first_name").value
+    //     console.log(FIRST_NAME)
+    // })
+    // document.addEventListener("scroll", (e)=>{
+    //     console.log(window.scrollY)
+    // })
 }
 
 // start point 
 document.addEventListener("DOMContentLoaded", () => {
-    // objects();
-    // const test = testDeclareFunction(2);
-    // arrFunction("Hello!!");
-    // conditionals();
-    loops();
+    // oop();
+    // events();
+
+    const CHANGE_BTN = document.querySelector("#button")
+    const HERO_TEXT = document.querySelector("#hero")
+    CHANGE_BTN.addEventListener("click", ()=>{
+        // HERO_TEXT.classList.toggle("changed")
+        // HERO_TEXT.setAttribute("test", "test")
+        // HERO_TEXT.style.color = "#f00"
+    })
+
 })
+
+
+
