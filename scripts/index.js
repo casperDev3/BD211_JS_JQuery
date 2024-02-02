f8730097939928ffe7bcf33400ad93c002004d07
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-02-02 19:17:02 +0200

    add

--- scripts/index.js
+++ scripts/index.js
@@ -1,123 +1,74 @@
-class Car {
-    #test = "test"
-    constructor(brand, year) {
-        this.brand = brand;
-        this.year = year;
-        this.#test = year + 2;
+/// ----
+// const BTN_PRESS = document.querySelector("#btn_press");
+// BTN_PRESS.addEventListener("click", (e) => {
+//     // window.close();
+//    console.log("History: ", window.history)
+// })
+
+// // window.open("https://www.google.com.ua/?hl=uk", "_blank")
+///----
+
+class Form{
+    constructor(){
+       this.#initElements(); 
+       this.#addListeners();
     }
-    //! public method
-    printText(txt) {
-        this.txt = txt;
-        console.log(txt)
+    #initElements(){
+        this.FIRST_NAME = document.querySelector("#first_name");
+        this.LAST_NAME = document.querySelector("#last_name");
+        this.PHONE = document.querySelector('#phone');
+        this.EMAIL = document.querySelector('#email');
+        this.SELECT = document.querySelector('#dist');
+        this.FILE = document.querySelector('#file');
+        // show places
+        this.SHOW_NAME = document.querySelector('#show_name');
+        this.SEND_BTN = document.querySelector('#send_btn');
     }
-    changeValTest(val) {
-        this.#test = val;
+    #addListeners(){
+        this.SEND_BTN.addEventListener("click", (e)=>{
+            e.preventDefault();
+            let data = this.#getDataFromFields();
+            console.log("__DATA", data)
+            const IS_VALID = this.#validateData(data);
+            IS_VALID ? this.#sendToServer(data) : alert("Field isn't valid!")
+        })
+        this.FILE.addEventListener("input", (e)=>{
+            const file = e.target.files[0]; // [[kjsdfjsd ]].flat()
+            let formData = new FormData();
+            formData.append("files", file)
+        })
+        this.FIRST_NAME.addEventListener("input", (e)=>{
+            // console.log(e.target.value);
+            this.SHOW_NAME.innerHTML = e.target.value;
+        })
     }
-    //! privare method 
-    #sayHello() {
-        console.log("Say Hello!")
+    #getDataFromFields(){
+        return {
+            first_name: this.FIRST_NAME.value,
+            last_name: this.LAST_NAME.value,
+            phone: this.PHONE.value,
+            email: this.EMAIL.value,
+            select: this.SELECT.value
+        }
     }
-    // getters
-    get test() {
-        return this.#test;
+    #validateData(data){
+        if(data.last_name.length < 3){
+            return false
+        }
+        return true
     }
-    // setter
-    set newTest(test) {
-        this.#test = test;
+    #sendToServer(data){
+        return {
+            status: true,
+            txt: "kldsjksjklfjlksd"
+        }
     }
 }
 
-class Tires extends Car {
-    constructor(brand, year) {
-        super(brand, year)
-    }
-    printText(txt = "custom") {
-        console.log(txt)
-    }
 
-}
 
-function oop() {
-    const car = new Car("BMW", 2012);
-    const tires = new Tires("Some brand", 2012)
-    console.log("tires", tires.brand, tires.year)
-    console.log(car.brand)
-    car.printText("text")
-    tires.printText()
-    console.log(car.test)
-    car.newTest = 2055
-    console.log(car.test)
-    car.changeValTest(2099)
-    console.log(car.test)
-    console.log(car.txt)
-}
 
-function events() {
-    // const BTN = document.getElementsByTagName("button");
-    // console.log(typeof BTN)
-    // BTN.map((btn)=>{
-    //     btn.addEventListener("click", ()=>{
-    //         alert("test")
-    //     })
-    // })
-
-    // //! GET ELEMENT BY ID
-    // const BTN = document.getElementById("button")
-    // BTN.addEventListener("click", ()=>{
-    //     alert("test ID")
-    // })
-
-    // //! GET ELEMENT BY CLASS -- TODO: Need search solution
-    // const BTNs = document.getElementsByClassName("btn");
-    // console.log(typeof BTNs)
-    // BTNs.addEventListener("click", ()=>{
-    //     console.log("test")
-    // })
-
-    // const BTN = document.querySelector("#button")
-    // BTN.addEventListener("click", ()=>{
-    //     alert("query ID")
-    // })
-
-    // const BTNs = document.querySelectorAll(".btn")
-    // BTNs.forEach((btn)=>{
-    //     btn.addEventListener("click", ()=>{
-    //         alert("test")
-    //     })
-    // })
-    // console.log(typeof BTNs)
-
-    // // ! Input event
-    // const FIRST_NAME = document.querySelector("#first_name")
-    // FIRST_NAME.addEventListener("input", (event)=>{
-    //     console.log(event.target.value)
-    // })
-
-    // const FORM = document.querySelector("#test_form")
-    // FORM.addEventListener("submit", (e)=>{
-    //     // e.preventDefault();
-    //     const FIRST_NAME = document.querySelector("#first_name").value
-    //     console.log(FIRST_NAME)
-    // })
-    // document.addEventListener("scroll", (e)=>{
-    //     console.log(window.scrollY)
-    // })
-}
-
-// start point 
-document.addEventListener("DOMContentLoaded", () => {
-    // oop();
-    // events();
-
-    const CHANGE_BTN = document.querySelector("#button")
-    const HERO_TEXT = document.querySelector("#hero")
-    CHANGE_BTN.addEventListener("click", ()=>{
-        // HERO_TEXT.classList.toggle("changed")
-        // HERO_TEXT.setAttribute("test", "test")
-        // HERO_TEXT.style.color = "#f00"
-    })
-
+// start point
+document.addEventListener("DOMContentLoaded", (e) => {
+    const f = new Form();
 })
-
-
-
