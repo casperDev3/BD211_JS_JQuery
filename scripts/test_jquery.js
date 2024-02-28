4408f40a732a4bbe0a431958bf9cdda829b9b844
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-02-28 19:54:42 +0200

    add second part

--- scripts/test_jquery.js
+++ scripts/test_jquery.js
@@ -11,10 +11,27 @@ $().ready(() => {
         // $("#phone_btn").addClass("active");
         $("#phone_btn").removeClass("active")
         $("#phone_btn").attr("data-fuck-attr", "80085");
-
+        $("#products_area").hide();
+        // $("#products_area").show();
+        $(".hero__filter_nav").html(
+            "<div>Test</div>"
+        )
     })
+    
 })
 
+
+$(document).ready(function(){
+    console.log("TEST SLICK SLIDER")
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
+      });
+  });
+
+// TODO: need delete or change on jQuery
 // const BTN = document.querySelector("#phone_btn");
 // BTN.addEventListener("click", ()=>{
 //     alert("You clicked btn!")
